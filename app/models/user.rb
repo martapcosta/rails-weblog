02a32ec848ac7bfad218f24904145fae98ac8640
Thibaud <thibaud@thibaud.gg>
2017-06-08 18:35:08 +0000

    test password min

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-
+  validates :password, length: { minimum: 8 }
+  
   def display_name
     if admin?
       "#{name} (admin)"
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,11 @@ class UserTest < ActiveSupport::TestCase
      user = User.new(admin: false, name:'jane')
      assert_equal("jane",user.display_name)
   end
+  test 'validates small password not valid' do
+    user = User.new(password: 'xxxxxx')
+
+    assert_not user.valid?
+    assert_equal [:password], user.errors.keys
+  end
+  
 end
